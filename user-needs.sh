fa4b0d244ea495e3f8b887847a6062acba9d4eed
Narasimha <murthyqa.n@gmail.com>
2025-10-19 17:32:23 +0530

    commit

---
+++ user-needs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#USER Variables
+echo "Current directory is : $PWD"
+echo "Who is running the script: $USER"
+echo "Home directory of user:$HOME"
+echo "PID of script: $$"
+slepp 10
+echo "PID of last command is:$!"
