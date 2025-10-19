a3e86809a5e0e4f538269602da0def146bac3b78
Narasimha <murthyqa.n@gmail.com>
2025-10-19 22:48:56 +0530

    commit

--- functions.sh
+++ functions.sh
@@ -2,21 +2,26 @@
 echo "Enter Number1"
 read NUMBER1
 echo "Enter Number2"
-read Number2
+read NUMBER2
 Validate() {
-    if [ $1 -lt $2 ]; then
+    if [ "$1" -lt "$2" ]; then
         echo "$2 is greater"
-    else
+    elif [ "$1" -gt "$2" ]; then
         echo "$1 is greater"
+    else
+        echo "Both are equal"
     fi
 }
 
 Verify() {
-    if [ $(($1 % $2)) -eq 0 ]; then
+    if [ $(($1 % 2)) -eq 0 ]; then
         echo "$1 is Even"
     else
-        echo "$2 is Odd"
+        echo "$1 is Odd"
     fi
 }
+
+# ✅ Pass actual values
 Validate "$NUMBER1" "$NUMBER2"
-Verify "$NUMBER1" 2
+Verify "$NUMBER1"
+Verify "$NUMBER2"
