225630cc440331288f57cac51c295795e9482246
Narasimha <murthyqa.n@gmail.com>
2025-10-19 22:47:10 +0530

    commit

--- functions.sh
+++ functions.sh
@@ -15,8 +15,8 @@ Verify() {
     if [ $(($1 % $2)) -eq 0 ]; then
         echo "$1 is Even"
     else
-        echo "$1 is Odd"
+        echo "$2 is Odd"
     fi
 }
 Validate "$NUMBER1" "$NUMBER2"
-Verify "$NUMBER1"
+Verify "$NUMBER1" 2
