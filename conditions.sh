a30fc9cad4166c34cf93fc17fd9567206b5819f2
Narasimha <murthyqa.n@gmail.com>
2025-10-19 17:41:48 +0530

    commit

---
+++ conditions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+NUMBER1=50
+NUMBER2=675
+echo "Numbers are:$NUMBER1,$NUMBER2
+if [$NUMBER1 -LT $NUMBER2] then
+    echo "$NUMBER2 is greater than $NUMBER1"
+else
+    echo "$NUMBER1 is greater than $NUMBER2"
+fi
