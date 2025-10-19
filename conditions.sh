a89c2e86b8eeabb6d3850affdb3f82c7f6fd1010
Narasimha <murthyqa.n@gmail.com>
2025-10-19 17:58:31 +0530

    commit

--- conditions.sh
+++ conditions.sh
@@ -4,14 +4,18 @@ read NUMBER1
 echo "Enter Number1"
 read NUMBER2
 echo "Numbers are:$NUMBER1,$NUMBER2"
-# if [ $NUMBER1 -lt $NUMBER2 ]; then
-#     echo "$NUMBER2 is greater than $NUMBER1"
-# else
-#     echo "$NUMBER1 is greater than $NUMBER2"
-# fi
+if [ $NUMBER1 -lt $NUMBER2 ]; then
+    echo "$NUMBER2 is greater than $NUMBER1"
+else
+    echo "$NUMBER1 is greater than $NUMBER2"
+fi
 
 if [ $((NUMBER1 % NUMBER2)) -eq 0 ]; then
     echo "$NUMBER1 is Even"
 else
     echo "$NUMBER1 is Odd"
+fi
+
+if $? -eq 0
+    echo "Previous command executes success"
 fi
