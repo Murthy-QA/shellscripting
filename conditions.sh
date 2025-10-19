357dc7d4366c43ce6c220a50e9e21d8eecd79ad5
Narasimha <murthyqa.n@gmail.com>
2025-10-19 17:43:02 +0530

    commit

--- conditions.sh
+++ conditions.sh
@@ -2,7 +2,7 @@
 NUMBER1=50
 NUMBER2=675
 echo "Numbers are:$NUMBER1,$NUMBER2"
-if [$NUMBER1 -LT $NUMBER2] then
+if [ $NUMBER1 -LT $NUMBER2 ]; then
     echo "$NUMBER2 is greater than $NUMBER1"
 else
     echo "$NUMBER1 is greater than $NUMBER2"
