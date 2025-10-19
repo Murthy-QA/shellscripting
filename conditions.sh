02379859b140ab07835c140f1fd0349f730986d2
Narasimha <murthyqa.n@gmail.com>
2025-10-19 17:44:43 +0530

    commit

--- conditions.sh
+++ conditions.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-NUMBER1=50
-NUMBER2=675
+echo "Enter Number1"
+read NUMBER1
+echo "Enter Number1"
+read NUMBER2
 echo "Numbers are:$NUMBER1,$NUMBER2"
 if [ $NUMBER1 -lt $NUMBER2 ]; then
     echo "$NUMBER2 is greater than $NUMBER1"
 else
     echo "$NUMBER1 is greater than $NUMBER2"
 fi
+
