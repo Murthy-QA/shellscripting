9337239707320f8fe7311471532dcc6ef4032ce8
Narasimha <murthyqa.n@gmail.com>
2025-10-20 12:18:31 +0530

    commit

--- Loops.sh
+++ Loops.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+count=1
 for i in 1 2 3;do
     echo "Number is:$i"
 done
 
-for i in {1..2..10}; do
+for i in {1..10}; do
     echo "Numbers from 1 to 10:$i"
+done
+
+while [ $count -le 3 ]; do
+    echo "Count is:$count"
+    ((count++))
 done
